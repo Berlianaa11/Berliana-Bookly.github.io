ecb218abf22ee43346eabf6bd1fc9922c5d64f8c
Berliana <133011155+Berlianaa11@users.noreply.github.com>
2023-07-17 15:55:37 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Beranda</a>
+         <a href="about.php">Tentang</a>
+         <a href="shop.php">Belanja</a>
+         <a href="contact.php">Hubungi Kami</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">Keranjang</a>
+         <a href="orders.php">Pemesanan</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +62 123-4567-8901 </p>
+         <p> <i class="fas fa-phone"></i> +62 111-2222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> kelompok1@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Sirnabaya, Karawang </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+   </div>
+   <div class="navbar-dark" style="background-color: var(--light-bg);">
+          <div class="copyright" style="color: var(--light-color); text-align: center; padding-top:50px; padding-bottom:5px; bottom:60px; width:100%; height:30px; font-size: 1.7rem;">Berliana Bookly Copyright © 2023</div>
+          </div>
+</section>
